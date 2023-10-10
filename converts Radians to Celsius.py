cc8cb8c5d91a0fe270f9c4a57177486611a77367
lemonntec <miraclechinedum1@gmail.com>
2023-10-10 12:13:49 +0100

    Radians to Celsuis

---
+++ converts Radians to Celsius.py
@@ -0,0 +1,6 @@
+#This program converts Radians to Celsius
+value = float(input('Input your value you wants to convert to celsuis: '))
+
+celsuis = int(value *(180/3.142))
+
+print('Your value',(value), 'is equal to:', celsuis, 'in degree celsuis')
